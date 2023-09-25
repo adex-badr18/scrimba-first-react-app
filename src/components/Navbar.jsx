f294a799ee31365227c9949d3793c7a8ca2bd05f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-25 11:41:49 +0100

    Rename imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import reactLogo from '../assets/react.svg'
+import logo from '../assets/react.svg'
 
 export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src={reactLogo} className='logo-img' alt="Logo" />
+                <img src={logo} className='logo-img' alt="Logo" />
                 <h3 className='logo-text'>ReactFacts</h3>
                 <h4 className='course-title'>React Course - Project 1</h4>
             </nav>
