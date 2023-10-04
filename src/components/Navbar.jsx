c4391adf3d55e01305611e94ac8eb0bcc671f46e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-04 13:13:45 +0100

    Refactor: Add toggle slider markup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,16 @@ export default function Navbar() {
             <nav>
                 <img src={logo} className='logo-img' alt="Logo" />
                 <h3 className='logo-text'>ReactFacts</h3>
-                <h4 className='course-title'>React Course - Project 1</h4>
+                
+                <div className="toggler">
+                    <p className="light-text">Light</p>
+
+                    <div className="toggler-slider">
+                        <div className="toggler-slider-circle"></div>
+                    </div>
+
+                    <p className="dark-text">Dark</p>
+                </div>
             </nav>
         </header>
     )
